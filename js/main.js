618099cba2b41bd5a52aecb4f0560ed1e3323820
yurimeeee <kplhn6@gmail.co>
2023-09-30 21:29:16 +0900

    [FEAT] slide 수정

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
-// $(document).ready(function() {
-
-// new fullpage("#fullpage", {
-//   //options here
-//   autoScrolling: true,
-//   scrollHorizontally: true,
-// });
-// });
 $(document).ready(function () {
   $("#fullpage").fullpage({
     //options here
     autoScrolling: true,
     scrollHorizontally: false,
-		// scrollOverflow: true,
-		normalScrollElements: '.normal-scroll',
-		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+    // scrollOverflow: true,
+    normalScrollElements: ".normal-scroll",
+    anchors: ["firstPage", "secondPage", "3rdPage", "4thpage", "lastPage"],
     // licenseKey: null
   });
 
   // 'normal-scroll' 클래스를 가진 섹션에 대해 일반 스크롤을 허용합니다.
-	// fullpage_api.setAllowScrolling(true, 'up, down', '.normal-scroll');
+  // fullpage_api.setAllowScrolling(true, 'up, down', '.normal-scroll');
   // AOS.init();
 
   //   new fullpage('#fullpage', {
@@ -26,4 +18,35 @@ $(document).ready(function () {
   // });
   // 	//methods
   // 	$.fn.fullpage.setAllowScrolling(false);
+
+  let work = document.querySelector(".sec_05");
+  let menu = document.querySelectorAll("#menu li");
+
+  function handleIntersection(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        menu.forEach((item) => item.classList.add("active"));
+      } else {
+        menu.forEach((item) => item.classList.remove("active"));
+      }
+    });
+  }
+
+  var options = {
+    root: null,
+    threshold: 0.8,
+  };
+
+  var observer = new IntersectionObserver(handleIntersection, options);
+  observer.observe(work);
+
+  // window.addEventListener("scroll", function () {
+  //   var scrollAmt = window.scrollY;
+  //   console.log('scrollAmt',scrollAmt)
+  //   if (scrollAmt > workHeight) {
+  //     menu.classList.add('active');
+  //   } else {
+  //     menu.classList.remove('active');
+  //   }
+  // });
 });
