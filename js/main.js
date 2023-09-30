35f458cca130d6023b5aad39815a0482bb787fde
yurimeeee <kplhn6@gmail.co>
2023-09-30 15:19:11 +0900

    [ADD] image 추

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
-
 // $(document).ready(function() {
-  
+
 // new fullpage("#fullpage", {
 //   //options here
 //   autoScrolling: true,
 //   scrollHorizontally: true,
 // });
 // });
-      $(document).ready(function() {
-	$('#fullpage').fullpage({
-		//options here
-		autoScrolling:true,
-		scrollHorizontally: false,
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: false,
+		// scrollOverflow: true,
+		normalScrollElements: '.normal-scroll',
+		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
     // licenseKey: null
-	});
+  });
 
+  // 'normal-scroll' 클래스를 가진 섹션에 대해 일반 스크롤을 허용합니다.
+	// fullpage_api.setAllowScrolling(true, 'up, down', '.normal-scroll');
+  // AOS.init();
 
-
-  AOS.init();
-
-//   new fullpage('#fullpage', {
-// 	anchors:['firstPage', 'secondPage', 'thirdPage']
-// });
-// 	//methods
-// 	$.fn.fullpage.setAllowScrolling(false);
+  //   new fullpage('#fullpage', {
+  // 	anchors:['firstPage', 'secondPage', 'thirdPage']
+  // });
+  // 	//methods
+  // 	$.fn.fullpage.setAllowScrolling(false);
 });
