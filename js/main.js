67714ed8d19a10cb62916bba0fe03843214a3060
yurimeeee <kplhn6@gmail.com>
2023-10-09 22:13:22 +0900

    [Add] favicon

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 $(document).ready(function () {
   $("#fullpage").fullpage({
     //options here
+    fitToSection: true,
+    scrollingSpeed: 1000,
     autoScrolling: true,
     scrollHorizontally: false,
     // scrollOverflow: true,
     normalScrollElements: ".normal-scroll",
-    anchors: ["firstPage", "secondPage", "3rdPage", "4thpage", "lastPage"],
+    anchors: [
+      "1stPage",
+      "2ndPage",
+      "2ndPage2",
+      "3rdPage",
+      "4thpage",
+      "5thpage",
+    ],
     // licenseKey: null
   });
 
@@ -49,4 +58,60 @@ $(document).ready(function () {
   //     menu.classList.remove('active');
   //   }
   // });
+  let skill = document.querySelector(".sec_04");
+
+  // function handleIntersection2(entries, observer) {
+  //   entries.forEach((entry) => {
+  //     if (entry.isIntersecting) {
+  //       menu.forEach((item) => item.classList.add("active"));
+  //     } else {
+  //       menu.forEach((item) => item.classList.remove("active"));
+  //     }
+  //   });
+  // }
+
+  // var options = {
+  //   root: null,
+  //   threshold: 0.8,
+  // };
+
+  // var observer2 = new IntersectionObserver(handleIntersection2, options);
+  // observer2.observe(skill);
+
+  let chart = $(".chart");
+  // let chartOST = chart.offset().top - 500;
+  // let isActive = false;
+
+  // $(window).scroll(function () {
+  //   if ($(this).scrollTop() > chartOST) {
+  //     if (!isActive) {
+  //       chartAnimation();
+  //       isActive = true;
+  //     }
+  //   }
+  // });
+
+  function chartAnimation() {
+    chart.each(function () {
+      // let $this = $(this);
+      // let title = $this.find("h2");
+      let targetNum = this.attr("data-percent");
+      let targetSVG = this.find(".outer");
+
+      $({ num: 0 }).animate(
+        { num: targetNum },
+        {
+          duration: 1500,
+          progress: function () {
+            let now = Math.ceil(this.num);
+            title.text(now);
+            let value = 537 - (537 * now) / 100;
+            targetSVG.css({ strokeDashoffset: value });
+          },
+        }
+      );
+    });
+  }
+
+  chartAnimation();
 });
